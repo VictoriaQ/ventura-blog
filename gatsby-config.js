0d4badd64e3f0a308f367552dd13ee7e189ae3c5
Victoria Quirante <victoria@limenius.com>
2019-05-13 23:34:17 +0200

    Change bio

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,11 +1,11 @@
 module.exports = {
   siteMetadata: {
-    title: `Gatsby Starter Blog`,
-    author: `Kyle Mathews`,
-    description: `A starter blog demonstrating what Gatsby can do.`,
-    siteUrl: `https://gatsby-starter-blog-demo.netlify.com/`,
+    title: `Ventura`,
+    author: `Victoria Quirante`,
+    description: `Un blog de aventuras.`,
+    siteUrl: `https://ysyry.netlify.com/`,
     social: {
-      twitter: `kylemathews`,
+      twitter: `vicqr`,
     },
   },
   plugins: [
